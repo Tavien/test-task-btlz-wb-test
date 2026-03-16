771e01b3d766fd569dedc7e1695298a3761efe84
kenaye <rchetin@mobilon.ru>
2026-03-16 19:35:19 +0700

    feat: add update all method in sheet service

--- src/services/spreadsheet.service.ts
+++ src/services/spreadsheet.service.ts
@@ -1,18 +1,43 @@
-import { SpreadsheetsRepository } from "#repositories/index.js";
+import { match, Result, type ResultType } from "#lib/result.js";
 
-import { Sheet } from "#types/index.js";
+import type { SpreadsheetsRepository, GoogleSheetsRepository } from "#repositories/index.js";
 
-export const createSpreadsheertService = (sheetRepository: SpreadsheetsRepository) => {
-    const addSheet = (sheet: Sheet) => (
-        sheetRepository.addSheet(sheet)
-    );
+import { AppError, Sheet, Tariff } from "#types/index.js";
 
-    const getSheets = () => (
-        sheetRepository.getAllSheets()
-    );
-    
-    return {
-        addSheet,
-        getSheets,
-    }
+export interface SpreadsheetService {
+    addSheet(sheet: Sheet): Promise<ResultType<Sheet, AppError>>;
+    getSheets(): Promise<ResultType<Sheet[], AppError>>;
+    updateAllSheets(tariffs: Tariff[]): Promise<ResultType<boolean[], AppError>[]>;
 }
+
+export const createSpreadsheertService = (sheetRepository: SpreadsheetsRepository, googleSheetsRepository: GoogleSheetsRepository)
+    : SpreadsheetService => {
+        const addSheet = (sheet: Sheet) => (
+            sheetRepository.addSheet(sheet)
+        );
+
+        const getSheets = () => (
+            sheetRepository.getAllSheets()
+        );
+
+        const updateAllSheets = async (tariffs: Tariff[]) => {
+            const sheetsResult = await getSheets();
+
+            const updatePromiseResult = match(sheetsResult)({
+                onOk: (sheets) => (
+                    sheets.map(sheet => (
+                        googleSheetsRepository.updateSheet(sheet, tariffs)
+                    ))
+                ),
+                onErr: (error) => Result.Err(error) as any
+            });
+
+            return Promise.all(updatePromiseResult);
+        }
+        
+        return {
+            addSheet,
+            getSheets,
+            updateAllSheets,
+        }
+    }
