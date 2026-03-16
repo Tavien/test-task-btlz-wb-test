ede825ac1fe126f780d9c8a37cdc0b590edbfb34
kenaye <rchetin@mobilon.ru>
2026-03-16 11:38:41 +0700

    feat: add db adapter type & knex adapter impl

---
+++ src/adapters/knex.adapter.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+import type { DbAdapter, QueryBuilder, ConflictBuilder } from "#types/index.js";
+
+export const createKnexAdapter = (knex: Knex): DbAdapter => {
+    const adapter = {
+        queryBuilder: <T>(table: string): QueryBuilder<T> => 
+            knex(table) as unknown as QueryBuilder<T>,
+        raw: <T>(sql: string, bindings?: any[]): Promise<T> =>
+            //@ts-ignore
+            knex.raw(sql, bindings) as unknown as Promise<T>,
+    };
+
+    return adapter as DbAdapter;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export * from './dbAdapter.interface.js'
