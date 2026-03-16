74b95ab44ce33a579287f3e8bd2440f26a696cca
kenaye <rchetin@mobilon.ru>
2026-03-16 23:14:44 +0700

    feat: add init & start jobs, add default spreasheet

--- src/postgres/seeds/spreadsheets.js
+++ src/postgres/seeds/spreadsheets.js
@@ -2,5 +2,8 @@
  * @param {import("knex").Knex} knex
  */
 export async function seed(knex) {
-    // dont need any seeds yet
+    await knex("spreadsheets")
+        .insert([{ spreadsheet_id: "1x1t_DgZO7fEwZzOA14H3uO9l1EUQiVLTWWIji993O4g" }])
+        .onConflict(["spreadsheet_id"])
+        .ignore();
 }
