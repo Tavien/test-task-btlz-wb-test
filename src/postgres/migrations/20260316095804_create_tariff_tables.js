96bfecade36bb76fbd774e2f3df8f8fca2a8959a
kenaye <rchetin@mobilon.ru>
2026-03-16 14:13:11 +0700

    fix: make some tariff fields nullable

--- src/postgres/migrations/20260316095804_create_tariff_tables.js
+++ src/postgres/migrations/20260316095804_create_tariff_tables.js
@@ -32,22 +32,22 @@ export async function up(knex) {
         table.increments("id").primary();
         table.date("date").notNullable();
 
-        table.string("warehouse_name", 255);
+        table.string("warehouse_name", 255).notNullable();
         table.string("geo_name", 255).notNullable();
 
         table.date("dt_next_box");
         table.date("dt_till_max").notNullable();
 
-        table.decimal("box_delivery_base", 10, 2).notNullable();
-        table.decimal("box_delivery_coef_expr", 10, 4).notNullable();
-        table.decimal("box_delivery_liter", 10, 2).notNullable();
-        table.decimal("box_delivery_marketplace_base", 10, 2).notNullable();
-        table.decimal("box_delivery_marketplace_coef_expr", 10, 4).notNullable();
-        table.decimal("box_delivery_marketplace_liter", 10, 2).notNullable();
+        table.decimal("box_delivery_base", 10, 2);
+        table.decimal("box_delivery_coef_expr", 10, 4);
+        table.decimal("box_delivery_liter", 10, 2);
+        table.decimal("box_delivery_marketplace_base", 10, 2);
+        table.decimal("box_delivery_marketplace_coef_expr", 10, 4);
+        table.decimal("box_delivery_marketplace_liter", 10, 2);
 
-        table.decimal("box_storage_base", 10, 2).notNullable();
-        table.decimal("box_storage_coef_expr", 10, 4).notNullable();
-        table.decimal("box_storage_liter", 10, 2).notNullable();
+        table.decimal("box_storage_base", 10, 2);
+        table.decimal("box_storage_coef_expr", 10, 4);
+        table.decimal("box_storage_liter", 10, 2);
 
         table.timestamps(true, true);
 
