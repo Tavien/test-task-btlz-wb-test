32c1dff48ace1ebebca432300fd1f73c79ae14b1
kenaye <rchetin@mobilon.ru>
2026-03-16 17:07:50 +0700

    feat: add general error type

--- src/types/error.types.ts
+++ src/types/error.types.ts
@@ -1,7 +1,10 @@
-import {RepositoryErrorCode, SerializerErrorCode} from "#constants/error-codes.js";
+import {RepositoryErrorCode, SerializerErrorCode, ServiceErrorCode} from "#constants/error-codes.js";
 import { BaseErr } from "#lib/result.js";
 
 export { RepositoryErrorCode };
 
+export type AppError = RepositoryErr | SerializerErr | SerializerErr;
+
 export type RepositoryErr = BaseErr<RepositoryErrorCode>;
 export type SerializerErr = BaseErr<SerializerErrorCode>;
+export type ServiceErr = BaseErr<ServiceErrorCode>;
